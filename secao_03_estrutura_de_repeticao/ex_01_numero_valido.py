8e8fa3792cd591e444d9f60ad4376dc4c3c4209e
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-31 14:07:01 -0300

    resposta exerc. 01 lista de repetição.

--- secao_03_estrutura_de_repeticao/ex_01_numero_valido.py
+++ secao_03_estrutura_de_repeticao/ex_01_numero_valido.py
@@ -36,7 +36,7 @@ def obter_numero_valido():
     """Escreva aqui em baixo a sua solução"""
     valido = 'não'
     while valido == 'não':
-        #nota = input('Entre com um número entre 0 e 10: ')
+        nota = input('Entre com um número entre 0 e 10: ')
         if not nota.isnumeric():
             print(f'Número inválido: {nota}')
         else: 
@@ -48,4 +48,4 @@ def obter_numero_valido():
     print(nota)
 
 
-#obter_numero_valido()
+obter_numero_valido()
