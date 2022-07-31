e9b25f351bdab3c112a9eb39609b880e89ad4c30
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-31 17:33:28 -0300

    resposta exerc. 06 lista de repetição.

--- secao_03_estrutura_de_repeticao/ex_06_numeros_1_a_20.py
+++ secao_03_estrutura_de_repeticao/ex_06_numeros_1_a_20.py
@@ -33,3 +33,16 @@ mostre os números um ao lado do outro.
 
 def escrever_numeros_de_1_a_20(formato:str) -> str:
     """Escreva aqui em baixo a sua solução"""
+    saida = ''
+    if formato == 'um_ao_lado_do_outro':
+        for linha in range(1,21):
+            saida += str(linha)
+            if linha < 20:
+                saida += " " 
+        print(saida)
+    if formato == 'um_abaixo_do_outro':
+        for linha in range(1,21):
+            print(linha)
+
+#escrever_numeros_de_1_a_20('um_ao_lado_do_outro')
+#escrever_numeros_de_1_a_20('um_abaixo_do_outro')
