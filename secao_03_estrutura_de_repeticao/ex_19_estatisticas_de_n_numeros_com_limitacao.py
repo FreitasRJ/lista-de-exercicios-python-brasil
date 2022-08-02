36c75542ea9b61704cce2bb3694453eb5c1539dc
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-08-02 17:26:41 -0300

    resposta exerc. 19 lista de repetição.

--- secao_03_estrutura_de_repeticao/ex_19_estatisticas_de_n_numeros_com_limitacao.py
+++ secao_03_estrutura_de_repeticao/ex_19_estatisticas_de_n_numeros_com_limitacao.py
@@ -24,3 +24,27 @@ Altere o programa anterior para que ele aceite apenas números entre 0 e 1000.
 
 def calcular_estatisticas(*numeros) -> str:
     """Escreva aqui em baixo a sua solução"""
+    tam = len(numeros)
+    soma = 0
+    mensagem = "'Maior valor: não existe. Menor valor: não existe. Soma: 0'"
+    if tam != 0:
+        if tam == 1:
+            maior = menor = soma = numeros[0]
+            mensagem = f"'Maior valor: {numeros[0]}. Menor valor: {numeros[0]}. Soma: {numeros[0]}'"
+        else:
+            for idx in range(tam):
+                soma += numeros[idx]
+                if not (0 <= numeros[idx] <= 1000):
+                    mensagem = "'Somente números de 0 a 1000 são permitidos'"
+                else:  
+                    maior = numeros[0]
+                    menor = numeros[1]
+                    for indx in range(tam):
+                        
+                        if maior < numeros[indx]:
+                            maior = numeros[indx]
+                        if menor > numeros[indx]:
+                            menor = numeros[indx] 
+                    mensagem = f"'Maior valor: {maior}. Menor valor: {menor}. Soma: {soma}'"
+      
+    print(mensagem)
