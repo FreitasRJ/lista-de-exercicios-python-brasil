a16a04cd9ce9b4a8cced45d61888ba6c8e1d6a1a
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-08-05 11:29:10 -0300

    resposta exerc. 25 lista de repetição.

--- secao_03_estrutura_de_repeticao/ex_25_classificador_de_turma.py
+++ secao_03_estrutura_de_repeticao/ex_25_classificador_de_turma.py
@@ -2,7 +2,8 @@
 Exercício 25 da seção de estrutura sequencial da Python Brasil:
 https://wiki.python.org.br/EstruturaDeRepeticao
 
-Faça um programa que peça para n pessoas a sua idade, ao final o programa devera verificar se a média de idade da turma
+Faça um programa que peça para n pessoas a sua idade, ao final o programa devera verificar se a média de idade da 
+turma
 varia entre 0 e 25,26 e 60 e maior que 60; e então, dizer se a turma é jovem, adulta ou idosa, conforme a média
 calculada.
 
@@ -21,6 +22,15 @@ Mostre a média de idade com uma casa decimal.
 
 """
 
-
+import statistics
 def classifcar_turma(*idades) -> str:
     """Escreva aqui em baixo a sua solução"""
+    media = statistics.mean(idades)
+    if 0 <= media <= 25:
+        classificacao = 'jovem'
+    elif 26 <= media < 60:
+        classificacao = 'adulta'
+    else:
+        classificacao = 'idosa'
+    return f'A turma é {classificacao}, pois a média é de {media:.1f} anos'
+   
