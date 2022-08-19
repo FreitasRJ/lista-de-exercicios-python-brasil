b449a39b75e2716dd89246471e70cc0141bf72ba
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-08-18 21:02:33 -0300

    resposta exerc. 18 lista de listas.

--- secao_04_exercicios_lista/ex_18_melhor_jogador.py
+++ secao_04_exercicios_lista/ex_18_melhor_jogador.py
@@ -2,7 +2,8 @@
 Exercício 18 da seção de listas da Python Brasil:
 https://wiki.python.org.br/ExerciciosListas
 
-Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores para saber qual o melhor jogador
+Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores para 
+saber qual o melhor jogador
 após cada jogo. Para isto, faz-se necessário o desenvolvimento de um programa, que será utilizado pelas telefonistas,
 para a computação dos votos. Para computar cada voto, a telefonista digitará um número, entre 1 e 23, correspondente ao
 número da camisa do jogador. Um número de jogador igual zero, indica que a votação foi encerrada.
@@ -44,7 +45,47 @@ outro número. Após o final da votação, o programa deverá exibir:
 """
 
 
+
+from itertools import count
+from typing import Counter
+from collections import Counter
+
 def receber_votos(*voto):
     """Escreva aqui em baixo a sua solução"""
+    votacao = []    
+    print('Enquete: Quem foi o melhor jogador?')
+    print('---------------------------------------------------------------')
 
+    for escolha in voto:
+            print(f'Número do jogador (0=fim): {escolha}')
+            if escolha == 0:
+                #return receber_votos(*votos)
+                break
+            if escolha < 1 or escolha > 23:
+                print("Informe um valor entre 1 e 23 ou 0 para sair!")
+                continue
+            if escolha > 0 and escolha < 24:
+               votacao.append(escolha)
 
+    cabecalho = ''
+    resultado = Counter(votacao)
+    tam = len(votacao)
+    
+    
+    cabecalho = '-' * 63 + '\n'
+    cabecalho += 'Resultado da votação:\n'
+    cabecalho += '-' * 63 + '\n'
+    cabecalho += f'Foram computados {tam} votos.\n'
+    cabecalho += '-' * 63 + '\n'
+    cabecalho += f'Jogador Votos           %'
+
+    print(cabecalho)
+    
+    for k,v in resultado.items():
+        ind = 100/tam
+        print(f'{k:<15d} {v} {ind * v:>18.1f}%')
+    melhor,_votacao = resultado.most_common(1)[0]
+
+    print(f'O melhor jogador foi o número {melhor}, com {_votacao} votos, correspondendo a {(_votacao * 100)/tam:.0f}% do total de votos.')
+    
+#receber_votos(9, 10, 9, 10, 11, 10, 50, 9, 9, 0)
